d9ed446926fb48ddd3916baf9ca5c10af60f29e6
CodeWizarz <dveloperbobby@gmail.com>
2023-02-08 00:12:02 +0530

    tst5

--- src/components/Characters/Pepe/Pepe.vue
+++ src/components/Characters/Pepe/Pepe.vue
@@ -257,15 +257,15 @@ export default {
     background: url('./assets/pepe-blink.png') 0 0 no-repeat;
     background-size: 100% 100%;
   }
-  .bg-head-ear {
-    top: 0;
-    left: 0;
-    z-index: 20;
-    width: 100%;
-    height: 100%;
-    background: url('./assets/head.png') 0 0 no-repeat;
-    background-size: 200% 100%;
-  }
+  // .bg-head-ear {
+  //   top: 0;
+  //   left: 0;
+  //   z-index: 20;
+  //   width: 100%;
+  //   height: 100%;
+  //   background: url('./assets/head.png') 0 0 no-repeat;
+  //   background-size: 200% 100%;
+  // }
   /*am/pa*/
   .bg-ampa_foliage_3,
   .bg-ampa_tree_2,
